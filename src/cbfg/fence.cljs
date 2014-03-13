3e3600b5d95114f8b5efcb4617231a096851701c
Steve Yen <steve.yen@gmail.com>
2014-03-13 11:09:22 -0700

    More concise param names.

--- src/cbfg/fence.cljs
+++ src/cbfg/fence.cljs
@@ -37,36 +37,36 @@
 ;; request format
 ;; {:rq function-to-call :fence true-or-false}
 
-(defn make-fenced-dispatcher [in-channel out-channel max-inflights]
+(defn make-fenced-dispatcher [in-ch out-ch max-inflights]
   (go-loop
       [inflights #{}            ; chans of requests currently being processed.
        fenced nil               ; chan of last, inflight "fenced" request.
        fenced-res nil]          ; last received result from last fenced request.
     (let [i (vec (if (or fenced ; if we're fenced or too many inflight requests,
                          (>= (count inflights) max-inflights))
-                   inflights    ; then ignore in-channel & finish existing inflight requests.
-                   (conj inflights in-channel)))
-          [v ch] (if (empty? i) ; empty when in-channel is closed and no inflights.
+                   inflights    ; then ignore in-ch & finish existing inflight requests.
+                   (conj inflights in-ch)))
+          [v ch] (if (empty? i) ; empty when in-ch is closed and no inflights.
                    [nil nil]
                    (alts! i))]
       (cond
-       (= nil v ch) (close! out-channel)
-       (= ch in-channel) (if (nil? v)
-                           (recur inflights out-channel nil)
-                           (let [new-inflight ((:rq v))]
-                             (recur (conj inflights new-inflight)
-                                    (if (:fence v) new-inflight nil)
-                                    nil)))
+       (= nil v ch) (close! out-ch)
+       (= ch in-ch) (if (nil? v)
+                      (recur inflights out-ch nil)
+                      (let [new-inflight ((:rq v))]
+                        (recur (conj inflights new-inflight)
+                               (if (:fence v) new-inflight nil)
+                               nil)))
        (= v nil) (let [new-inflights (disj inflights ch)] ; an inflight request is done.
                    (if (empty? new-inflights)
                      (do (when fenced-res                 ; all inflight requests are done, so we can
-                           (>! out-channel fenced-res))   ; send the fenced-res that we've been keeping.
+                           (>! out-ch fenced-res))        ; send the fenced-res that we've been keeping.
                          (recur new-inflights nil nil))
                      (recur new-inflights fenced fenced-res)))
        (= ch fenced) (do (when fenced-res
-                           (>! out-channel fenced-res))   ; send off any previous fenced-res so
+                           (>! out-ch fenced-res))        ; send off any previous fenced-res so
                          (recur inflights fenced v))      ; we can keep v as our latest fenced-res.
-       :else (do (>! out-channel v)
+       :else (do (>! out-ch v)
                  (recur inflights fenced fenced-res))))))
 
 ;; ------------------------------------------------------------
