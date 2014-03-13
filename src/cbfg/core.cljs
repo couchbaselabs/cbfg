085168ac9b2223bbcff27d3218853a439abb4562
Steve Yen <steve.yen@gmail.com>
2014-03-13 09:03:17 -0700

    No om for now.

--- src/cbfg/core.cljs
+++ src/cbfg/core.cljs
@@ -1,74 +1,26 @@
 (ns cbfg.core
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require cbfg.ddl
-            [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [goog.dom :as dom]
+            [goog.events :as events]
+            [cljs.core.async :refer [<! put! chan]]))
 
 (enable-console-print!)
 
-(print cbfg.ddl/hi)
+(defn listen [el type]
+  (let [out (chan)]
+    (events/listen el type (fn [e] (put! out e)))
+    out))
 
-(defn foo [a b]
-  (+ a b))
+(let [clicks (listen (dom/getElement "go") "click")]
+  (go (while true
+        (println (<! clicks))
+        (println (user-input)))))
 
-(. js/console (log "Hello world!" (foo 1 2)))
+(println (dom/getElement "input"))
 
-;; ------------------------------------------------
-
-(def bucket-kinds
-  {:memcached {:cap 'cp
-               :allowed-ops [:get :set :del :arith :append]}
-   :couchbase {:cap 'cp
-               :allowed-ops [:get :set :del :arith :append]}
-   :couchdb   {:cap 'ap
-               :allowed-ops [:get :cas-set :cas-del]}})
-
-;; cfg stuff is admininistrator chosen values.
-;; cfg stuff has uuid'ed / version / parent.
-
-(def cfg-model {:name "name your model cfg"
-                :clusters {}})
-(def cfg-cluster {:parent-model nil
-                  :name "west"})
-(def cfg-bucket {:parent-cluster nil
-                 :name "default"
-                 :kind 'couchbase
-                 :num-partitions 0
-                 :per-node-mem-quota 0
-                 :indexes {}
-                 :storage-name "default"
-                 :replica-count 0
-                 :replica-rules []}) ; TODO: user/group/auth?
-(def cfg-index {:parent-bucket nil
-                :name "byCity"
-                :path "address.city"
-                :aggregate nil
-                :storage-name "default"}) ; TODO: view?
-(def cfg-node {:parent-cluster nil
-               :name "aaa"
-               :port 0
-               :container "" ; for zone/row/rack/shelf awareness.
-               :usage []
-               :weight 0.0})
-(def map-partitions {})
-
-(def node {:cfg-node {}
-           :memory {}
-           :processors 4
-           :storages {}
-           :buckets {}
-           :stats {}})
-(def node-storage {})
-
-(def node-bucket {:cfg-bucket {}
-                  :node-collections {}})
-(def node-collection {:node-partitions {}})
-(def node-partition {})
-
-(def write-queue {})
-(def read-queue {})
-
-(def send-queue {})
-(def recv-queue {})
+(defn user-input []
+  (.-value (dom/getElement "input")))
 
 ;; ------------------------------------------------
 
@@ -79,18 +31,3 @@
           :past []
           :upcoming {}})
 
-;; ------------------------------------------------
-
-(def app-state
-  (atom {:text "Hello world!"}))
-
-(defn app-ui [app-state owner]
-  (reify
-    om/IRender
-    (render [_]
-      (dom/div nil
-               (dom/h1 nil (:text app-state))))))
-
-(om/root app-state
-         app-ui
-         (. js/document (getElementById "app")))
