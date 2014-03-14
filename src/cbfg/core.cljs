8d9bc9dec2724f776bf9ad2b9c83fe66425b58ec
Steve Yen <steve.yen@gmail.com>
2014-03-14 11:38:29 -0700

    Some tick-speed prep work.

--- src/cbfg/core.cljs
+++ src/cbfg/core.cljs
@@ -2,8 +2,8 @@
 ;; generic stuff should go elsewhere.
 
 (ns cbfg.core
-  (:require-macros [cljs.core.async.macros :refer [go]]
-                   [cbfg.ago :refer [ago]])
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]]
+                   [cbfg.ago :refer [ago atake]])
   (:require [clojure.string :as string]
             [cljs.core.async :refer [<! put! chan]]
             [goog.dom :as dom]
@@ -23,8 +23,19 @@
     (events/listen el type (fn [e] (put! out e)))
     out))
 
-(let [clicks (listen (dom/getElement "go") "click")]
-  (ago world nil
+(def tick-speed (atom 1000))
+
+(defn change-tick-speed [s]
+  (reset! tick-speed s))
+
+(let [clicks (listen (dom/getElement "go") "click")
+      ticks (chan)
+      w {:ticks ticks}]
+  (go-loop [t 0]
+    (<! (timeout @tick-speed))
+    (>! ticks t)
+    (recur (inc t)))
+  (ago world w
        (while true
          (<! clicks)
          (set! (.-innerHTML (dom/getElement "output"))
