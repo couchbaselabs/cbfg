cda7128698f89e0d5ef7e6eb4dbc135675b925eb
Steve Yen <steve.yen@gmail.com>
2014-03-13 17:43:15 -0700

    Debugging println's.

--- src/cbfg/ago.clj
+++ src/cbfg/ago.clj
@@ -1,5 +1,5 @@
 (ns cbfg.ago
-    (:require [cljs.core.async.macros :refer [go go-loop]]))
+  (:require [cljs.core.async.macros :refer [go]]))
 
 (defmacro ago [child-actx-binding-name actx & body]
   `(let [~child-actx-binding-name [~actx '~child-actx-binding-name (gensym)]]
@@ -9,17 +9,22 @@
   `(ago ~child-actx-binding-name ~actx (loop ~bindings ~@body)))
 
 (defmacro achan [actx]
-  `(cljs.core.async/chan))
+  `(do (println "achan" ~actx)
+       (cljs.core.async/chan)))
 
 (defmacro achan-buf [actx buf-or-size]
-  `(cljs.core.async/chan ~buf-or-size))
+  `(do (println "achan-buf" ~actx ~buf-or-size)
+       (cljs.core.async/chan ~buf-or-size))
 
 (defmacro aclose [actx ch]
-  `(cljs.core.async/close! ~ch))
+  `(do (println "aclose" ~actx)
+       (cljs.core.async/close! ~ch)))
 
 (defmacro atake [actx ch]
-  `(cljs.core.async/<! ~ch))
+  `(do (println "atake" ~actx)
+       (cljs.core.async/<! ~ch)))
 
 (defmacro aput [actx ch msg]
-  `(cljs.core.async/>! ~ch ~msg))
+  `(do (println "aput" ~actx)
+       (cljs.core.async/>! ~ch ~msg)))
 
