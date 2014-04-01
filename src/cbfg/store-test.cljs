3b9758f3f2be0c651f184763eede46af3c65e498
Steve Yen <steve.yen@gmail.com>
2014-04-01 10:42:54 -0700

    More concise test harness.

--- src/cbfg/store-test.cljs
+++ src/cbfg/store-test.cljs
@@ -3,17 +3,16 @@
   (:require [cljs.core.async :refer [into]]
             [cbfg.store :as store]))
 
-(defn e [num result expect]
-  (println (swap! num inc) ":"
+(defn e [n result expect]
+  (println (swap! n inc) ":"
            (if (= result expect) "pass" (str "FAIL:" result expect)))
   (= result expect))
 
 (defn test [actx opaque-id]
-  (let [num (atom 0)
+  (let [n (atom 0)
         s (store/make-store)]
     (ago test actx
-         (if (and (let [ch (store/store-get test s 1 "does-not-exist")]
-                    (e num (atake test ch)
-                       {:status :not-found, :key "does-not-exist", :opaque 1})))
+         (if (and (e n (atake test (store/store-get test s 1 "does-not-exist"))
+                     {:status :not-found, :key "does-not-exist", :opaque 1}))
            "pass"
-           (str "FAIL: on test #" @num)))))
+           (str "FAIL: on test #" @n)))))
