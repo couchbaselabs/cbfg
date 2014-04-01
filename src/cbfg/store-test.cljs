6ea136b782ee6cef6efd1583a6d7f86a4a05e85b
Steve Yen <steve.yen@gmail.com>
2014-04-01 11:30:53 -0700

    First set test.

--- src/cbfg/store-test.cljs
+++ src/cbfg/store-test.cljs
@@ -4,11 +4,15 @@
             [cbfg.store :as store]))
 
 (defn e [n result expect]
-  (println (str (swap! n inc) ":")
-           (if (= result expect)
-             (str "pass: " (:key expect))
-             (str "FAIL:" result expect)))
-  (= result expect))
+  (let [result2 (if (:cas expect)
+                  result
+                  (dissoc result :cas))
+        pass (= result2 expect)]
+    (println (str (swap! n inc) ":")
+             (if pass
+               (str "pass: " (:key expect))
+               (str "FAIL:" result2 expect)))
+    pass))
 
 (defn test [actx opaque-id]
   (ago test actx
@@ -25,6 +29,8 @@
                   (e n (atake test (store/store-set test s 5 "does-not-exist" 0 "" :append))
                      {:status :not-found, :key "does-not-exist", :opaque 5})
                   (e n (atake test (store/store-set test s 6 "does-not-exist" 0 "" :replace))
-                     {:status :not-found, :key "does-not-exist", :opaque 6}))
+                     {:status :not-found, :key "does-not-exist", :opaque 6})
+                  (e n (atake test (store/store-set test s 7 "does-not-exist" 0 "7" :set))
+                     {:status :ok, :key "does-not-exist", :opaque 7, :sq 2}))
            "pass"
            (str "FAIL: on test #" @n)))))
