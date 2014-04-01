ca2026bc8422e17cff9dc7bf6648b2128fb6beba
Steve Yen <steve.yen@gmail.com>
2014-04-01 11:24:03 -0700

    Some more store tests.

--- src/cbfg/store-test.cljs
+++ src/cbfg/store-test.cljs
@@ -4,15 +4,23 @@
             [cbfg.store :as store]))
 
 (defn e [n result expect]
-  (println (swap! n inc) ":"
-           (if (= result expect) "pass" (str "FAIL:" result expect)))
+  (println (str (swap! n inc) ":")
+           (if (= result expect)
+             (str "pass: " (:key expect))
+             (str "FAIL:" result expect)))
   (= result expect))
 
 (defn test [actx opaque-id]
   (ago test actx
        (let [n (atom 0)
-             s (store/make-store)]
+             s (store/make-store test)]
          (if (and (e n (atake test (store/store-get test s 1 "does-not-exist"))
-                     {:status :not-found, :key "does-not-exist", :opaque 1}))
+                     {:status :not-found, :key "does-not-exist", :opaque 1})
+                  (e n (atake test (store/store-get test s 2 "does-not-exist2"))
+                     {:status :not-found, :key "does-not-exist2", :opaque 2})
+                  (e n (atake test (store/store-del test s 3 "does-not-exist" 0))
+                     {:status :not-found, :key "does-not-exist", :opaque 3})
+                  (e n (atake test (store/store-set test s 4 "does-not-exist" 0 "" :replace))
+                     {:status :not-found, :key "does-not-exist", :opaque 4}))
            "pass"
            (str "FAIL: on test #" @n)))))
