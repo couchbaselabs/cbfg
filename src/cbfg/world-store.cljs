6aeda7d125bdb9fcb5de65be79117d7c53abbbd1
Steve Yen <steve.yen@gmail.com>
2014-04-01 15:56:11 -0700

    Basic render-client-cmds ported to world-store from world-fence.

--- src/cbfg/world-store.cljs
+++ src/cbfg/world-store.cljs
@@ -5,7 +5,41 @@
             [cbfg.fence :refer [make-fenced-pump]]
             [cbfg.store :as store]
             [cbfg.store-test]
-            [cbfg.vis :refer [vis-init listen-el get-el-value log-el]]))
+            [cbfg.vis :refer [vis-init listen-el
+                              get-el-value set-el-innerHTML log-el]]))
+
+(defn filter-r [r] (if (map? r)
+                     (-> r
+                         (dissoc :opaque)
+                         (dissoc :op)
+                         (dissoc :fence))
+                     r))
+
+(defn render-client-cmds [client-cmds]
+  (set-el-innerHTML "client"
+                    (apply str
+                           (flatten ["<table>"
+                                     "<tr><th>id</th><th>fence</th><th>op</th>"
+                                     "    <th>args</th><th>responses</th></tr>"
+                                     (map (fn [[opaque [request responses]]]
+                                            ["<tr class='"
+                                             (when (some #(:result (second %)) responses)
+                                               "complete") "'>"
+                                             " <td>" opaque "</td>"
+                                             " <td>" (:fence request) "</td>"
+                                             " <td>" (:op request) "</td>"
+                                             " <td>" (filter-r request) "</td>"
+                                             " <td class='responses'><ul>"
+                                             (map (fn [[out-time response]]
+                                                    ["<li style='margin-left: " out-time "em;'>"
+                                                     (filter-r response)
+                                                     "</li>"])
+                                                  (reverse responses))
+                                             "</ul></td>"
+                                             "</tr>"])
+                                          (sort #(compare (first %1) (first %2))
+                                                client-cmds))
+                                     "</table>"]))))
 
 (defn make-store-cmd-handlers [store]
   {"get"
@@ -74,10 +108,15 @@
                                                  cmd3 (reduce #(assoc %1 (keyword %2)
                                                                       (get-el-value (str op "-" %2)))
                                                               cmd2 params)]
+                                             (render-client-cmds (swap! client-cmds
+                                                                        #(assoc % num-ins [cmd3 nil])))
                                              (log-el el-prefix "input" (str num-ins ": " cmd3))
                                              (aput client in-ch (handler cmd3))
                                              (recur (inc num-ins) num-outs))
                              (= ch out-ch) (let [result v]
+                                             (render-client-cmds (swap! client-cmds
+                                                                        #(update-in % [(:opaque v) 1]
+                                                                                    conj [num-outs v])))
                                              (log-el el-prefix "output" (str num-outs ": " result))
                                              (recur num-ins (inc num-outs))))))
                 (make-fenced-pump world in-ch out-ch @store-max-inflight)))
