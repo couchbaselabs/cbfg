be8106d5d9bef99c4b6679f6b13756b5a1c821aa
Steve Yen <steve.yen@gmail.com>
2014-04-02 09:10:26 -0700

    Added store-set-do helper func.

--- src/cbfg/store.cljs
+++ src/cbfg/store.cljs
@@ -26,49 +26,51 @@
            {:opaque opaque :status :not-found
             :key key}))))
 
+(defn store-set-do [store opaque key val op cas-check old-cas]
+  (let [s1 (assoc store :next-sq (max (:next-sq store)
+                                      (inc (:max-deleted-sq store))))
+        new-sq (:next-sq s1)
+        old-sq (get-in s1 [:keys key])]
+    (if (and (= op :add) old-sq)
+      [store {:opaque opaque :status :exists :key key}]
+      (if (and (or (= op :replace)
+                   (= op :append)
+                   (= op :prepend))
+               (not old-sq))
+        [store {:opaque opaque :status :not-found :key key}]
+        (let [prev-change (when (or cas-check
+                                    (= op :append)
+                                    (= op :prepend))
+                            (get-in s1 [:changes old-sq]))]
+          (if (and cas-check (not= old-cas (:cas prev-change)))
+            [store {:opaque opaque :status :wrong-cas :key key}]
+            (let [new-cas (gen-cas)
+                  new-val (case op
+                            :add val
+                            :replace val
+                            :append (str (:val prev-change) val)
+                            :prepend (str val (:val prev-change))
+                            val)]
+              [(-> s1
+                   (dissoc-in [:changes old-sq])
+                   (assoc-in [:keys key] new-sq)
+                   (assoc-in [:changes new-sq]
+                             {:key key :sq new-sq :cas new-cas
+                              :val new-val})
+                   (update-in [:next-sq] inc))
+               {:opaque opaque :status :ok
+                :key key :sq new-sq :cas new-cas}])))))))
+
+
 (defn store-set [actx store opaque key old-cas val op]
   (ago store-set actx
        (let [cas-check (and old-cas (not (zero? old-cas)))]
          (if (and cas-check (= op :add))
            {:opaque opaque :status :wrong-cas :key key}
            (let [res (atom nil)]
-             (swap! store
-                    #(let [s1 (assoc % :next-sq (max (:next-sq %)
-                                                     (inc (:max-deleted-sq %))))
-                           new-sq (:next-sq s1)
-                           old-sq (get-in s1 [:keys key])]
-                       (if (and (= op :add) old-sq)
-                         (return % res
-                                 {:opaque opaque :status :exists :key key})
-                         (if (and (or (= op :replace)
-                                      (= op :append)
-                                      (= op :prepend))
-                                  (not old-sq))
-                           (return % res
-                                   {:opaque opaque :status :not-found :key key})
-                           (let [prev-change (when (or cas-check
-                                                       (= op :append)
-                                                       (= op :prepend))
-                                               (get-in s1 [:changes old-sq]))]
-                             (if (and cas-check (not= old-cas (:cas prev-change)))
-                               (return % res
-                                       {:opaque opaque :status :wrong-cas :key key})
-                               (let [new-cas (gen-cas)
-                                     new-val (case op
-                                               :add val
-                                               :replace val
-                                               :append (str (:val prev-change) val)
-                                               :prepend (str val (:val prev-change))
-                                               val)]
-                                 (return (-> s1
-                                             (dissoc-in [:changes old-sq])
-                                             (assoc-in [:keys key] new-sq)
-                                             (assoc-in [:changes new-sq]
-                                                       {:key key :sq new-sq :cas new-cas
-                                                        :val new-val})
-                                             (update-in [:next-sq] inc))
-                                         res {:opaque opaque :status :ok
-                                              :key key :sq new-sq :cas new-cas}))))))))
+             (swap! store #(let [[s1 r1] (store-set-do % opaque key val op
+                                                       cas-check old-cas)]
+                             (return s1 res r1)))
              @res)))))
 
 (defn store-del [actx store opaque key old-cas]
