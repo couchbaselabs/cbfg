8a141b8779d1363f165fe4d8273cd66fcea04a69
Steve Yen <steve.yen@gmail.com>
2014-04-02 22:53:03 -0700

    Using simpler chs test for fence loop exit.

--- src/cbfg/fence.cljs
+++ src/cbfg/fence.cljs
@@ -37,34 +37,33 @@
 
 (defn make-fenced-pump [actx in-ch out-ch max-inflight]
   (ago-loop fenced-pump actx
-   [inflight-chs #{}              ; chans of requests currently being processed.
-    fenced-ch nil                 ; chan of last, inflight "fenced" request.
-    fenced-ch-res nil]            ; last received result from last fenced request.
-   (let [i (vec (if (or fenced-ch ; if we're fenced or too many inflight requests,
+   [inflight-chs #{}                ; chans of requests currently being processed.
+    fenced-ch nil                   ; chan of last, inflight "fenced" request.
+    fenced-ch-res nil]              ; last received result from last fenced request.
+   (let [chs (vec (if (or fenced-ch ; if we're fenced or too many inflight requests,
                         (>= (count inflight-chs) max-inflight))
-                  inflight-chs    ; then ignore in-ch & finish existing inflight requests.
-                  (conj inflight-chs in-ch)))
-         [v ch] (if (empty? i)    ; empty when in-ch is closed and no inflight-chs.
-                  [nil nil]
-                  (aalts fenced-pump i))]
-     (cond
-      (= nil v ch) (aclose fenced-pump out-ch)
-      (= ch in-ch) (if (nil? v)
-                     (recur inflight-chs out-ch nil)
-                     (let [new-inflight ((:rq v) fenced-pump)]
-                       (recur (conj inflight-chs new-inflight)
-                              (when (:fence v) new-inflight)
-                              nil)))
-      (= v nil) (let [new-inflight-chs (disj inflight-chs ch)] ; an inflight request is done.
-                  (if (empty? new-inflight-chs)                ; if inflight requests are all done,
-                    (do (when fenced-ch-res                    ; finally send the fenced-ch-res that
-                          (aput fenced-pump                    ; we have been holding onto.
-                                out-ch fenced-ch-res))
-                        (recur new-inflight-chs nil nil))
-                    (recur new-inflight-chs fenced-ch fenced-ch-res)))
-      (= ch fenced-ch) (do (when fenced-ch-res
-                          (aput fenced-pump            ; send any previous fenced-res so
-                                out-ch fenced-ch-res)) ; we can hold v as last fenced-ch-res.
-                        (recur inflight-chs fenced-ch v))
-      :else (do (aput fenced-pump out-ch v)
-                (recur inflight-chs fenced-ch fenced-ch-res))))))
+                  inflight-chs      ; then ignore in-ch & finish existing inflight requests.
+                  (conj inflight-chs in-ch)))]
+     (if (seq chs)                  ; empty when in-ch is closed and no inflight-chs.
+       (let [[v ch] (aalts fenced-pump chs)]
+         (cond
+          (= ch in-ch) (if (nil? v)
+                         (recur inflight-chs out-ch nil)           ; using out-ch as a sentinel.
+                         (let [new-inflight ((:rq v) fenced-pump)]
+                           (recur (conj inflight-chs new-inflight)
+                                  (when (:fence v) new-inflight)
+                                  nil)))
+          (= v nil) (let [new-inflight-chs (disj inflight-chs ch)] ; an inflight request is done.
+                      (if (empty? new-inflight-chs)                ; if inflight requests are all done,
+                        (do (when fenced-ch-res                    ; finally send the fenced-ch-res
+                              (aput fenced-pump                    ; that we have been holding onto.
+                                    out-ch fenced-ch-res))
+                            (recur new-inflight-chs nil nil))
+                        (recur new-inflight-chs fenced-ch fenced-ch-res)))
+          (= ch fenced-ch) (do (when fenced-ch-res
+                                 (aput fenced-pump            ; send any previous fenced-res so
+                                       out-ch fenced-ch-res)) ; we can hold v as last fenced-ch-res.
+                               (recur inflight-chs fenced-ch v))
+          :else (do (aput fenced-pump out-ch v)
+                    (recur inflight-chs fenced-ch fenced-ch-res))))
+       (aclose fenced-pump out-ch)))))
