d82ee65929f87f26c2a209fdfa96e693a59418d7
Steve Yen <steve.yen@gmail.com>
2014-04-03 16:59:32 -0700

    Reuse start-test in grouper-test.

--- src/cbfg/world-grouper.cljs
+++ src/cbfg/world-grouper.cljs
@@ -1,12 +1,7 @@
 (ns cbfg.world-grouper
-  (:require-macros [cbfg.ago :refer [ago]])
-  (:require [cljs.core.async :refer [chan <! sliding-buffer]]
-            cbfg.grouper-test))
+  (:require cbfg.grouper-test
+            [cbfg.world-base :refer [start-test]]))
 
 ; TODO: No standalone world simulation / visualization for grouper yet.
 
-(let [last-id (atom 0)
-      gen-id #(swap! last-id inc)]
-  (ago grouper-test-actx [{:gen-id gen-id :event-ch (chan (sliding-buffer 1))}]
-       (println "grouper-test:"
-                (<! (cbfg.grouper-test/test grouper-test-actx 0)))))
+(start-test "grouper-test" cbfg.grouper-test/test)
