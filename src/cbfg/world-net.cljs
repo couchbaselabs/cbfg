2e564e27541568ed31795345a3f73c037ff826f4
Steve Yen <steve.yen@gmail.com>
2014-04-07 23:20:16 -0700

    Added placeholder echo server.

--- src/cbfg/world-net.cljs
+++ src/cbfg/world-net.cljs
@@ -1,5 +1,5 @@
 (ns cbfg.world-net
-  (:require-macros [cbfg.ago :refer [ago-loop achan-buf aalts aput]])
+  (:require-macros [cbfg.ago :refer [ago ago-loop achan achan-buf aalts aput atake]])
   (:require [cljs.core.async :refer [chan]]
             [cbfg.vis :refer [vis-init get-el-value]]
             [cbfg.net :refer [make-net]]
@@ -8,10 +8,8 @@
                                      render-client-hist start-test]]))
 
 (def cmd-handlers
-  {"add"    (fn [c] (assoc c :rq #(cbfg.world-base/example-add % c)))
-   "sub"    (fn [c] (assoc c :rq #(cbfg.world-base/example-sub % c)))
-   "count"  (fn [c] (assoc c :rq #(cbfg.world-base/example-count % c)))
-   "test"   (fn [c] (assoc c :rq #(cbfg.fence-test/test % (:opaque c))))})
+  {"echo" (fn [c] (assoc c :rq #(cbfg.world-base/example-add % c)))
+   "test" (fn [c] (assoc c :rq #(cbfg.net-test/test % (:opaque c))))})
 
 (def max-inflight (atom 10))
 
@@ -19,14 +17,13 @@
   (let [cmd-inject-ch (chan)
         cmd-ch (replay-cmd-ch cmd-inject-ch (keys cmd-handlers)
                               (fn [ev] {:op (.-id (.-target ev))
-                                        :x (js/parseInt (get-el-value "x"))
-                                        :y (js/parseInt (get-el-value "y"))
-                                        :delay (js/parseInt (get-el-value "delay"))
-                                        :fence (= (get-el-value "fence") "1")}))
+                                        :msg (get-el-value "msg")}))
         client-hist (atom {})] ; Keyed by opaque -> [request, replies].
     (vis-init (fn [world vis-chs]
                 (let [in-ch (achan-buf world 100)
-                      out-ch (achan-buf world 0)]
+                      out-ch (achan-buf world 0)
+                      listen-ch (achan-buf world 10)
+                      connect-ch (achan-buf world 10)]
                   (ago-loop client world [num-ins 0 num-outs 0]
                             (let [[v ch] (aalts client [cmd-ch out-ch])
                                   ts (+ num-ins num-outs)]
@@ -44,7 +41,22 @@
                                                (do (render-client-hist (swap! client-hist
                                                                               #(update-in % [(:opaque v) 1]
                                                                                           conj [ts v])))
-                                                   (recur num-ins (inc num-outs)))))))))
+                                                   (recur num-ins (inc num-outs)))))))
+                  (ago server-init world
+                       (let [listen-result-ch (achan server-init)]
+                         (aput server-init listen-ch [:server 8000 listen-result-ch])
+                         (let [[close-accept-ch accept-ch] (atake server-init listen-result-ch)]
+                           (ago-loop server-accept-loop world
+                                     [num-accepts 0]
+                                     (let [[server-send-ch server-recv-ch close-server-recv-ch]
+                                           (atake server-accept-loop accept-ch)]
+                                       (ago-loop server-conn-loop server-accept-loop
+                                                 [num-requests 0]
+                                                 (aput server-conn-loop server-send-ch
+                                                       (atake server-conn-loop server-recv-ch))
+                                                 (recur (inc num-requests)))
+                                       (recur (inc num-accepts)))))))
+                  (make-net world listen-ch connect-ch)))
               el-prefix nil init-event-delay)
     cmd-inject-ch))
 
