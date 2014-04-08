30b482a512038b059d0d28d40a375a14c5155c38
Steve Yen <steve.yen@gmail.com>
2014-04-08 07:46:28 -0700

    More net world.

--- src/cbfg/world-net.cljs
+++ src/cbfg/world-net.cljs
@@ -20,43 +20,56 @@
                                         :msg (get-el-value "msg")}))
         client-hist (atom {})] ; Keyed by opaque -> [request, replies].
     (vis-init (fn [world vis-chs]
-                (let [in-ch (achan-buf world 100)
-                      out-ch (achan-buf world 0)
+                (let [connect-ch (achan-buf world 10)
                       listen-ch (achan-buf world 10)
-                      connect-ch (achan-buf world 10)]
-                  (ago-loop client world [num-ins 0 num-outs 0]
-                            (let [[v ch] (aalts client [cmd-ch out-ch])
-                                  ts (+ num-ins num-outs)]
-                              (cond
-                               (= ch cmd-ch) (if (= (:op v) "replay")
-                                               (world-replay in-ch vis-chs world-vis-init el-prefix
-                                                             @client-hist (:replay-to v))
-                                               (let [cmd (assoc-in v [:opaque] ts)
-                                                     cmd-rq ((get cmd-handlers (:op cmd)) cmd)]
+                      listen-result-ch (achan world)]
+                  (make-net world listen-ch connect-ch)
+                  (ago init-world world
+                       (aput init-world listen-ch [:server 8000 listen-result-ch])
+                       (when-let [[close-accept-ch accept-ch] (atake init-world listen-result-ch)]
+                         (println "server is listening")
+                         (ago-loop server-accept-loop world
+                                   [num-accepts 0]
+                                   (println "server is accepting")
+                                   (when-let [[server-send-ch server-recv-ch close-server-recv-ch]
+                                              (atake server-accept-loop accept-ch)]
+                                     (println "server accepted")
+                                     (ago-loop server-conn-loop server-accept-loop
+                                               [num-requests 0]
+                                               (println "server is recv'ing")
+                                               (let [msg (atake server-conn-loop server-recv-ch)]
+                                                 (println "server echoing" msg)
+                                                 (aput server-conn-loop server-send-ch msg)
+                                                 (recur (inc num-requests))))
+                                     (recur (inc num-accepts))))
+                         (ago client-init world
+                              (let [connect-result-ch (achan client-init)]
+                                (aput client-init connect-ch [:server 8000 :client connect-result-ch])
+                                (when-let [[client-send-ch client-recv-ch close-client-recv-ch]
+                                           (atake client-init connect-result-ch)]
+                                  (println "client is connected")
+                                  (ago-loop client-loop world
+                                            [num-requests 0 num-responses 0]
+                                            (let [[v ch] (aalts client-loop [cmd-ch client-recv-ch])
+                                                  ts (+ num-requests num-responses)]
+                                              (println "client got event" v)
+                                              (cond
+                                               (= ch cmd-ch)
+                                               (if (= (:op v) "replay")
+                                                 (world-replay client-send-ch vis-chs world-vis-init el-prefix
+                                                               @client-hist (:replay-to v))
+                                                 (let [cmd (assoc-in v [:opaque] ts)
+                                                       cmd-rq ((get cmd-handlers (:op cmd)) cmd)]
+                                                   (render-client-hist (swap! client-hist
+                                                                              #(assoc % ts [cmd nil])))
+                                                   (aput client-loop client-send-ch cmd-rq)
+                                                   (recur (inc num-requests) num-responses)))
+                                               (= ch client-recv-ch)
+                                               (when v
                                                  (render-client-hist (swap! client-hist
-                                                                            #(assoc % ts [cmd nil])))
-                                                 (aput client in-ch cmd-rq)
-                                                 (recur (inc num-ins) num-outs)))
-                               (= ch out-ch) (when v
-                                               (do (render-client-hist (swap! client-hist
-                                                                              #(update-in % [(:opaque v) 1]
-                                                                                          conj [ts v])))
-                                                   (recur num-ins (inc num-outs)))))))
-                  (ago server-init world
-                       (let [listen-result-ch (achan server-init)]
-                         (aput server-init listen-ch [:server 8000 listen-result-ch])
-                         (when-let [[close-accept-ch accept-ch] (atake server-init listen-result-ch)]
-                           (ago-loop server-accept-loop world
-                                     [num-accepts 0]
-                                     (when-let [[server-send-ch server-recv-ch close-server-recv-ch]
-                                                (atake server-accept-loop accept-ch)]
-                                       (ago-loop server-conn-loop server-accept-loop
-                                                 [num-requests 0]
-                                                 (aput server-conn-loop server-send-ch
-                                                       (atake server-conn-loop server-recv-ch))
-                                                 (recur (inc num-requests)))
-                                       (recur (inc num-accepts)))))))
-                  (make-net world listen-ch connect-ch)))
+                                                                            #(update-in % [(:opaque v) 1]
+                                                                                        conj [ts v])))
+                                                 (recur num-requests (inc num-responses)))))))))))))
               el-prefix nil init-event-delay)
     cmd-inject-ch))
 
