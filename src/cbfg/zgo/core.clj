dab9652b8afed2587ce52dda4bf3310f3454b363
Steve Yen <steve.yen@gmail.com>
2014-04-12 14:31:50 -0700

    Added line-range func.

--- src/cbfg/zgo/core.clj
+++ src/cbfg/zgo/core.clj
@@ -4,21 +4,15 @@
             [clojure.tools.reader.reader-types :as rt]
             [clojure.pprint :refer [pprint]]))
 
-(defn prm [x]
+(defn pprintm [x]
   (binding [*print-meta* true]
-    (pr x)))
-
-(defn read-model-file-rdr [fname rdr]
-  (binding [*read-eval* false]
-    (loop [forms []]
-      (let [form (r/read rdr false :eof)]
-        (if (= form :eof)
-          forms
-          (recur (conj forms form)))))))
+    (pprint x)))
 
 (defn read-model-file [fname]
-    {:forms (let [rdr (rt/indexing-push-back-reader
-                       (java.io.PushbackReader. (clojure.java.io/reader fname)) 10 fname)]
+    {:fname fname
+     :forms (let [rdr (rt/indexing-push-back-reader
+                       (java.io.PushbackReader. (clojure.java.io/reader fname))
+                       10 fname)]
               (binding [*read-eval* false]
                 (loop [forms []]
                   (let [form (r/read rdr false :eof)]
@@ -28,10 +22,37 @@
      :lines (with-open [rdr (clojure.java.io/reader fname)]
               (vec (line-seq rdr)))})
 
+(def MAX-LINE 0x8FFFFFF)
+(def MIN-LINE -1)
+
+(defn line-range [form]
+  (if (coll? form)
+    (reduce (fn [[min-line-beg max-line-end] v]
+              (let [[line-beg line-end] (line-range v)]
+                [(min min-line-beg line-beg)
+                 (max max-line-end line-end)]))
+            [MAX-LINE MIN-LINE]
+            form)
+    [(or (:line (meta form)) MAX-LINE)
+     (or (:end-line (meta form)) MIN-LINE)]))
+
+(defn process-top-level-form [model form]
+  (let [[op name & rest] form]
+    (case (str op)
+      "ns" ["package" name (line-range form)]
+      "defn" ["func" name (line-range form)]
+      "UNKNOWN-TOP-LEVEL-FORM")))
+
+(defn process-top-level-forms [model]
+  (map #(process-top-level-form model %)
+       (:forms model)))
+
 (defn -main
   "some docstring"
   []
   (println "hello world from zgo")
-  (pprint (read-model-file "src/cbfg/misc.cljs")))
-
+  (let [model (read-model-file "src/cbfg/fence.cljs")
+        pmodel (process-top-level-forms model)]
+    (pprintm model)
+    (pprintm pmodel)))
 
