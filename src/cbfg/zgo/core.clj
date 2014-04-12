5c5f5a299258bce6be31304ac62f45e17ec75e36
Steve Yen <steve.yen@gmail.com>
2014-04-12 12:48:34 -0700

    Better read-model-file func.

--- src/cbfg/zgo/core.clj
+++ src/cbfg/zgo/core.clj
@@ -1,32 +1,37 @@
 (ns cbfg.zgo.core
   (:gen-class)
   (:require [clojure.tools.reader :as r]
-            [clojure.tools.reader.reader-types :as rt]))
+            [clojure.tools.reader.reader-types :as rt]
+            [clojure.pprint :refer [pprint]]))
 
 (defn prm [x]
   (binding [*print-meta* true]
     (pr x)))
 
-(defn read-file [fname]
-  (let [ipbr (rt/indexing-push-back-reader
-              (java.io.PushbackReader. (clojure.java.io/reader fname)) 10 fname)]
-    (binding [*read-eval* false]
-      (let [r (r/read ipbr false :eof)]
-        (prm r))
-      (let [r (r/read ipbr false :eof)]
-        (prm r))
-      (let [r (r/read ipbr false :eof)]
-        (prm r))
-      (let [r (r/read ipbr false :eof)]
-        (prm r))
-      (let [r (r/read ipbr false :eof)]
-        (prm r))
-        )))
+(defn read-model-file-rdr [fname rdr]
+  (binding [*read-eval* false]
+    (loop [forms []]
+      (let [form (r/read rdr false :eof)]
+        (if (= form :eof)
+          forms
+          (recur (conj forms form)))))))
+
+(defn read-model-file [fname]
+    {:forms (let [rdr (rt/indexing-push-back-reader
+                       (java.io.PushbackReader. (clojure.java.io/reader fname)) 10 fname)]
+              (binding [*read-eval* false]
+                (loop [forms []]
+                  (let [form (r/read rdr false :eof)]
+                    (if (= form :eof)
+                      forms
+                      (recur (conj forms form)))))))
+     :lines (with-open [rdr (clojure.java.io/reader fname)]
+              (vec (line-seq rdr)))})
 
 (defn -main
   "some docstring"
   []
   (println "hello world from zgo")
-  (read-file "src/cbfg/misc.cljs"))
+  (pprint (read-model-file "src/cbfg/misc.cljs")))
 
 
