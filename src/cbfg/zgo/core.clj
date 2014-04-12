6f359461f0a1feec93901508037a401f5a697dc8
Steve Yen <steve.yen@gmail.com>
2014-04-11 17:46:43 -0700

    Playing with indexing-push-back-reader.

--- src/cbfg/zgo/core.clj
+++ src/cbfg/zgo/core.clj
@@ -1,8 +1,32 @@
 (ns cbfg.zgo.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.tools.reader :as r]
+            [clojure.tools.reader.reader-types :as rt]))
+
+(defn prm [x]
+  (binding [*print-meta* true]
+    (pr x)))
+
+(defn read-file [fname]
+  (let [ipbr (rt/indexing-push-back-reader
+              (java.io.PushbackReader. (clojure.java.io/reader fname)) 10 fname)]
+    (binding [*read-eval* false]
+      (let [r (r/read ipbr false :eof)]
+        (prm r))
+      (let [r (r/read ipbr false :eof)]
+        (prm r))
+      (let [r (r/read ipbr false :eof)]
+        (prm r))
+      (let [r (r/read ipbr false :eof)]
+        (prm r))
+      (let [r (r/read ipbr false :eof)]
+        (prm r))
+        )))
 
 (defn -main
   "some docstring"
   []
-  (println "hello world from zgo"))
+  (println "hello world from zgo")
+  (read-file "src/cbfg/misc.cljs"))
+
 
