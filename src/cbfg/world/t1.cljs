bf8404d03972f61966037d2b9a3bdfa8ee747aa0
Steve Yen <steve.yen@gmail.com>
2014-05-02 15:07:18 -0700

    Trying multiple render worlds.

--- src/cbfg/world/t1.cljs
+++ src/cbfg/world/t1.cljs
@@ -11,7 +11,6 @@
    {:text "hello"
     :controls ["play" "step" "pause"]
     :speed 1.0
-    :now 0
     :events [1 2 3 4]
     :world {:a 11 :b 22 :c 33}
     :hover nil}))
@@ -31,21 +30,30 @@
 (def ghost-state
   (atom nil))
 
-(defn vinit [el-id]
+(defn render-world [app owner]
+  (apply dom/ul nil
+         (map (fn [[k v]] (dom/li nil (str k ":" v)))
+              (:world app))))
+
+(defn init-roots [app-state]
   (om/root
    (fn [app owner]
      (apply dom/ul nil
-            (map (fn [text] (dom/li nil (dom/button nil text)))
+            (map (fn [x] (dom/li nil (dom/button nil x)))
                  (:controls app))))
    app-state
    {:target (. js/document (getElementById "controls"))})
   (om/root
    (fn [app owner]
-     (dom/div nil (:text app)))
+     (apply dom/ul nil
+            (map (fn [x] (dom/li nil x))
+                 (:events app))))
    app-state
-   {:target (. js/document (getElementById el-id))}))
+   {:target (. js/document (getElementById "events"))})
+  (om/root render-world app-state
+           {:target (. js/document (getElementById "world"))})
+  (om/root render-world app-state
+           {:target (. js/document (getElementById "world-map"))}))
 
 (defn world-vis-init [el-prefix init-event-delay]
-  (vinit el-prefix)
-  (vinit "world"))
-
+  (init-roots app-state))
