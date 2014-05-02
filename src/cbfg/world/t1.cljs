3d879dca727e90bff3daaba8b3a86c69e3b8a47b
Steve Yen <steve.yen@gmail.com>
2014-05-02 14:43:31 -0700

    More placeholder t1 UI.

--- src/cbfg/world/t1.cljs
+++ src/cbfg/world/t1.cljs
@@ -4,26 +4,41 @@
             [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]))
 
+;; How to assign locations to world entities before rendering?
+
 (def app-state
   (atom
    {:text "hello"
     :controls ["play" "step" "pause"]
     :speed 1.0
-    :curr-time 0
-    :inputs []
-    :snapshots []
-    :list ["Lion" "Zebra" "Buffalo" "Antelope"]}))
+    :now 0
+    :events [1 2 3 4]
+    :world {:a 11 :b 22 :c 33}
+    :hover nil}))
+
+(def app-history
+  (atom []))
+
+(def app-future
+  (atom []))
 
 (def transient-state
   (atom nil))
 
 (def preview-state
-  (atom nil))
+  (atom {:focus nil}))
 
 (def ghost-state
   (atom nil))
 
 (defn vinit [el-id]
+  (om/root
+   (fn [app owner]
+     (apply dom/ul nil
+            (map (fn [text] (dom/li nil (dom/button nil text)))
+                 (:controls app))))
+   app-state
+   {:target (. js/document (getElementById "controls"))})
   (om/root
    (fn [app owner]
      (dom/div nil (:text app)))
@@ -32,5 +47,5 @@
 
 (defn world-vis-init [el-prefix init-event-delay]
   (vinit el-prefix)
-  (vinit "preview"))
+  (vinit "world"))
 
