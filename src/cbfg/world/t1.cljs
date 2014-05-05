36c823b9f1caceeb8c816e6fa0de5bc4d6d4d73d
Steve Yen <steve.yen@gmail.com>
2014-05-05 08:55:23 -0700

    More helper root funcs.

--- src/cbfg/world/t1.cljs
+++ src/cbfg/world/t1.cljs
@@ -26,10 +26,122 @@
                   [14 :event [:d 44]]
                   [20 :snapshot]
                   [21 :event [:a 21]]
+                  [22 :event [:b 22]]
+
+                  [0 :snapshot]
+                  [1 :event [:a 1]]
+                  [2 :event [:b 2]]
+                  [3 :event [:c 3]]
+                  [4 :event [:d 4]]
+                  [10 :snapshot]
+                  [11 :event [:a 11]]
+                  [12 :event [:b 22]]
+                  [13 :event [:c 33]]
+                  [14 :event [:d 44]]
+                  [20 :snapshot]
+                  [21 :event [:a 21]]
+                  [22 :event [:b 22]]
+
+                  [0 :snapshot]
+                  [1 :event [:a 1]]
+                  [2 :event [:b 2]]
+                  [3 :event [:c 3]]
+                  [4 :event [:d 4]]
+                  [10 :snapshot]
+                  [11 :event [:a 11]]
+                  [12 :event [:b 22]]
+                  [13 :event [:c 33]]
+                  [14 :event [:d 44]]
+                  [20 :snapshot]
+                  [21 :event [:a 21]]
+                  [22 :event [:b 22]]
+
+                  [0 :snapshot]
+                  [1 :event [:a 1]]
+                  [2 :event [:b 2]]
+                  [3 :event [:c 3]]
+                  [4 :event [:d 4]]
+                  [10 :snapshot]
+                  [11 :event [:a 11]]
+                  [12 :event [:b 22]]
+                  [13 :event [:c 33]]
+                  [14 :event [:d 44]]
+                  [20 :snapshot]
+                  [21 :event [:a 21]]
+                  [22 :event [:b 22]]
+
+                  [0 :snapshot]
+                  [1 :event [:a 1]]
+                  [2 :event [:b 2]]
+                  [3 :event [:c 3]]
+                  [4 :event [:d 4]]
+                  [10 :snapshot]
+                  [11 :event [:a 11]]
+                  [12 :event [:b 22]]
+                  [13 :event [:c 33]]
+                  [14 :event [:d 44]]
+                  [20 :snapshot]
+                  [21 :event [:a 21]]
+                  [22 :event [:b 22]]
+
+                  [0 :snapshot]
+                  [1 :event [:a 1]]
+                  [2 :event [:b 2]]
+                  [3 :event [:c 3]]
+                  [4 :event [:d 4]]
+                  [10 :snapshot]
+                  [11 :event [:a 11]]
+                  [12 :event [:b 22]]
+                  [13 :event [:c 33]]
+                  [14 :event [:d 44]]
+                  [20 :snapshot]
+                  [21 :event [:a 21]]
+                  [22 :event [:b 22]]
+
+                  [0 :snapshot]
+                  [1 :event [:a 1]]
+                  [2 :event [:b 2]]
+                  [3 :event [:c 3]]
+                  [4 :event [:d 4]]
+                  [10 :snapshot]
+                  [11 :event [:a 11]]
+                  [12 :event [:b 22]]
+                  [13 :event [:c 33]]
+                  [14 :event [:d 44]]
+                  [20 :snapshot]
+                  [21 :event [:a 21]]
+                  [22 :event [:b 22]]
+
+                  [0 :snapshot]
+                  [1 :event [:a 1]]
+                  [2 :event [:b 2]]
+                  [3 :event [:c 3]]
+                  [4 :event [:d 4]]
+                  [10 :snapshot]
+                  [11 :event [:a 11]]
+                  [12 :event [:b 22]]
+                  [13 :event [:c 33]]
+                  [14 :event [:d 44]]
+                  [20 :snapshot]
+                  [21 :event [:a 21]]
+                  [22 :event [:b 22]]
+
+                  [0 :snapshot]
+                  [1 :event [:a 1]]
+                  [2 :event [:b 2]]
+                  [3 :event [:c 3]]
+                  [4 :event [:d 4]]
+                  [10 :snapshot]
+                  [11 :event [:a 11]]
+                  [12 :event [:b 22]]
+                  [13 :event [:c 33]]
+                  [14 :event [:d 44]]
+                  [20 :snapshot]
+                  [21 :event [:a 21]]
                   [22 :event [:b 22]]]}))
 
 (def app-world
-  (atom {:a 11 :b 22 :c 33 :d 44}))
+  (atom {:a 10 :b 20 :c 30 :d 40}))
 
 (def app-world-hover
   (atom nil))
@@ -46,53 +158,52 @@
 (defn event-focus [snapshot-ts event-ts]
   (println :event-focus snapshot-ts event-ts)
   (when-let [ss (get-in @app-history [:snapshots snapshot-ts])]
+    (println "AAAAAAA" ss)
     (reset! app-world-hover ss)))
 
 (defn event-blur [snapshot-ts event-ts]
   (println :event-blur snapshot-ts event-ts)
   (reset! app-world-hover nil))
 
-(defn init-roots []
-  (om/root
-   (fn [app owner]
+(defn render-events [app owner]
+  (apply dom/ul nil
+         (second
+          (reduce
+           (fn [[last-snapshot-ts res] [ts kind args]]
+             (if (= kind :snapshot)
+               [ts
+                (conj res
+                      (dom/li #js {:onMouseEnter #(event-focus ts nil)
+                                   :onMouseLeave #(event-blur ts nil)}
+                              (str ts)
+                              (render-snapshot app owner ts)))]
+               [(or last-snapshot-ts ts)
+                (conj res
+                      (dom/li #js {:onMouseEnter
+                                   #(event-focus last-snapshot-ts ts)
+                                   :onMouseLeave
+                                   #(event-blur last-snapshot-ts ts)}
+                              (str ts (pr-str args))))]))
+           [nil []]
+           (:events app)))))
+
+(defn render-controls[app owner]
      (apply dom/ul nil
             (map (fn [x] (dom/li nil (dom/button nil x)))
                  (:controls app))))
-   app-controls
-   {:target (. js/document (getElementById "controls"))})
-
-  (om/root
-   (fn [app owner]
-     (apply dom/ul nil
-            (second
-             (reduce
-              (fn [[last-snapshot-ts res] [ts kind args]]
-                (if (= kind :snapshot)
-                  [ts
-                   (conj res
-                         (dom/li #js {:onMouseEnter #(event-focus ts nil)
-                                      :onMouseLeave #(event-blur ts nil)}
-                                 (str ts)
-                                 (render-snapshot app owner ts)))]
-                  [last-snapshot-ts
-                   (conj res
-                         (dom/li #js {:onMouseEnter
-                                      #(event-focus last-snapshot-ts ts)
-                                      :onMouseLeave
-                                      #(event-blur last-snapshot-ts ts)}
-                                 (str ts (pr-str args))))]))
-              [nil []]
-              (:events app)))))
-   app-history
-   {:target (. js/document (getElementById "events"))})
-
+(defn init-roots []
+  (om/root render-controls app-controls
+           {:target (. js/document (getElementById "controls"))})
+  (om/root render-events app-history
+           {:target (. js/document (getElementById "events"))})
   (om/root render-world app-world
            {:target (. js/document (getElementById "world"))})
   (om/root render-world app-world
            {:target (. js/document (getElementById "world-map"))})
-
   (om/root render-world app-world-hover
-           {:target (. js/document (getElementById "world-hover"))}))
+           {:target (. js/document (getElementById "world-hover"))})
+  (om/root render-world app-world-hover
+           {:target (. js/document (getElementById "world-map-hover"))}))
 
 (defn world-vis-init [el-prefix init-event-delay]
   (init-roots))
