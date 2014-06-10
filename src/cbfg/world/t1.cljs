5862fe5a48c92f91017052afe5946d7c58e44254
Steve Yen <steve.yen@gmail.com>
2014-06-09 17:37:01 -0700

    Renamed world to prog, with prog-init.

--- src/cbfg/world/t1.cljs
+++ src/cbfg/world/t1.cljs
@@ -13,18 +13,20 @@
             [cbfg.world.lane]
             [cbfg.world.base :refer [world-cmd-loop]]))
 
-(def prog-base (atom {})) ; { :world => world-actx
-                          ;   :net-listen-ch => ch
-                          ;   :net-connect-ch => ch
-                          ;   :res-ch => ch }
+(def prog-base    (atom {}))  ; Stable parts of prog.
+(def prog-curr    (atom {}))  ; The current prog-frame.
+(def prog-hover   (atom nil)) ; A past prog-frame while hovering over prog-history.
+(def prog-history (atom []))  ; Each event is [ts label prog-frame].
 
-(def prog-curr ; The current prog-frame.
-  (atom {:servers {}    ; { server-addr => ports }.
-         :clients {}})) ; { client-addr => client-info }.
-
-(def prog-hover (atom nil)) ; A past prog-frame while hovering over prog-history.
-
-(def prog-history (atom [])) ; Each event is [ts label prog-frame].
+(defn prog-init [world]
+  (reset! prog-base {:world world
+                     :net-listen-ch (achan-buf world 10)
+                     :net-connect-ch (achan-buf world 10)
+                     :res-ch (achan-buf world 10)})
+  (reset! prog-curr {:servers {}   ; { server-addr => ports }.
+                     :clients {}}) ; { client-addr => client-info }.
+  (reset! prog-hover nil)
+  (reset! prog-history []))
 
 ; -------------------------------------------------------------------
 
@@ -35,11 +37,11 @@
 
 (defn on-prog-frame-focus [ts label prog-frame]
   (reset! prog-hover prog-frame)
-  (.add gdom/classes (gdom/getElement "world-container") "hover"))
+  (.add gdom/classes (gdom/getElement "prog-container") "hover"))
 
 (defn on-prog-frame-blur []
   (reset! prog-hover nil)
-  (.remove gdom/classes (gdom/getElement "world-container") "hover"))
+  (.remove gdom/classes (gdom/getElement "prog-container") "hover"))
 
 (defn render-events [app owner]
   (apply dom/ul nil
@@ -57,13 +59,13 @@
 
 (defn init-roots []
   (om/root render-prog-frame prog-curr
-           {:target (. js/document (getElementById "world"))})
+           {:target (. js/document (getElementById "prog"))})
   (om/root render-prog-frame prog-curr
-           {:target (. js/document (getElementById "world-map"))})
+           {:target (. js/document (getElementById "prog-map"))})
   (om/root render-prog-frame prog-hover
-           {:target (. js/document (getElementById "world-hover"))})
+           {:target (. js/document (getElementById "prog-hover"))})
   (om/root render-prog-frame prog-hover
-           {:target (. js/document (getElementById "world-map-hover"))})
+           {:target (. js/document (getElementById "prog-map-hover"))})
   (om/root render-events prog-history
            {:target (. js/document (getElementById "events"))})
   (om/root render-clients prog-curr
@@ -99,16 +101,13 @@
                                  }}
                   :chs {} ; {ch -> {:id (gen-id), :msgs {msg -> true},
                           ;         :first-taker-actx actx-or-nil}}.
-                  :gen-id gen-id})
-            delayed-event-cb (fn [vis-next]
-                               (println :on-delayed-event-cb @last-id))]
-        (reset! prog-base {:world world
-                           :net-listen-ch (achan-buf world 10)
-                           :net-connect-ch (achan-buf world 10)
-                           :res-ch (achan-buf world 10)})
+                  :gen-id gen-id})]
+        (prog-init world)
         (cbfg.vis/process-events vis event-delay cbfg.vis/vis-event-handlers
                                  event-ch step-ch event-run-ch)
-        (cbfg.vis/process-render el-prefix world event-run-ch delayed-event-cb)
+        (cbfg.vis/process-render el-prefix world event-run-ch
+                                 (fn [vis-next]
+                                   (println :on-delayed-event-cb @last-id)))
         (make-net world
                   (:net-listen-ch @prog-base)
                   (:net-connect-ch @prog-base))
@@ -126,9 +125,10 @@
                            (merge (map #(listen-el (gdom/getElement %) "click")
                                        (keys cbfg.world.lane/cmd-handlers))))
               expand-ch (listen-el (gdom/getElement (str el-prefix "-html")) "click")
-              prog-in (get-el-value "prog")
+              prog-in (get-el-value "prog-in")
               prog-js (str "with (cbfg.world.t1) {" prog-in "}")
               prog-res (try (js/eval prog-js) (catch js/Object ex ex))]
+          (println :prog-res prog-res)
           (world-cmd-loop world cbfg.world.lane/cmd-handlers cmd-ch
                           req-ch (:res-ch @prog-base)
                           vis-chs world-vis-init el-prefix)
@@ -138,16 +138,15 @@
                                  (get-in @prog-curr [:clients (:client msg) :req-ch])]
                         (aput cmd-dispatch-loop client-req-ch msg))
                       (recur (inc num-dispatches))))
-          (println :prog-res prog-res)
-          (println :prog-history prog-history)
           (go-loop [] ; Process expand/collapse UI events.
-            (let [actx-id (cbfg.vis/no-prefix (.-id (.-target (<! expand-ch))))]
-              (doseq [[actx actx-info] (:actxs @vis)]
-                (when (= actx-id (last actx))
-                  (swap! vis #(assoc-in % [:actxs actx :collapsed]
-                                        (not (:collapsed actx-info))))
-                  (>! event-run-ch [@vis nil true nil])))
-              (recur)))
+            (when-let [ev (<! expand-ch)]
+              (let [actx-id (cbfg.vis/no-prefix (.-id (.-target ev)))]
+                (doseq [[actx actx-info] (:actxs @vis)]
+                  (when (= actx-id (last actx))
+                    (swap! vis #(assoc-in % [:actxs actx :collapsed]
+                                          (not (:collapsed actx-info))))
+                    (>! event-run-ch [@vis nil true nil])))
+                (recur))))
           (<! prog-ch)
           (close! expand-ch)
           (close! cmd-ch)
