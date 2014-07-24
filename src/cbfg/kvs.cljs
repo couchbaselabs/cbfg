0e800877f6c593beb9ae1dc206659bca3ca868ee
Steve Yen <steve.yen@gmail.com>
2014-07-24 13:46:12 -0700

    More plausible kvs multi-get.

--- src/cbfg/kvs.cljs
+++ src/cbfg/kvs.cljs
@@ -19,82 +19,88 @@
 ; TODO: Add snapshot ability.
 ; TODO: Readers not blocked by writers.
 
+(defn make-kc [] ; A kc is a keys / changes map.
+  {:keys (sorted-map)      ; key -> sq
+   :changes (sorted-map)}) ; [sq key] -> {:key k, :sq s, :deleted true | :val v}
+
 (defn make-kvs [name uuid]
   {:name name
    :uuid uuid
-   :keys (sorted-map)    ; key -> sq
-   :changes (sorted-map) ; [sq key] -> {:key k, :sq s, :deleted true | :val v}
+   :clean (make-kc) ; Read-only.
+   :dirty (make-kc) ; Mutations go here, until "persisted / made durable".
    :next-sq 1})
 
-(defn kvs-sq-by-key [kvs key]
-  (get (:keys kvs) key))
+(defn kc-sq-by-key [kc key]
+  (get (:keys kc) key))
 
-(defn kvs-entry-by-key-sq [kvs key sq]
-  (get (:changes kvs) [sq key]))
+(defn kc-entry-by-key-sq [kc key sq]
+  (get (:changes kc) [sq key]))
 
-(defn kvs-entry-by-key [kvs key]
-  (kvs-entry-by-key-sq kvs key (kvs-sq-by-key kvs key)))
+(defn kc-entry-by-key [kc key]
+  (kc-entry-by-key-sq kc key (kc-sq-by-key kc key)))
 
-(defn kvs-check [kvs-ident work-fn]
+(defn kvs-ident-check [kvs-ident work-fn]
+  ; Wrapper that checks to see if kvs-ident refers to a current kvs.
   (fn [state] ; Meant to be used as work-fn for state-work function.
     (if-let [[name uuid] kvs-ident]
       (if-let [kvs (get-in state [:kvss name])]
         (if (= uuid (:uuid kvs))
           (work-fn state kvs)
-          [state {:status :mismatch :sub-status :wrong-uuid}])
-        [state {:status :not-found :sub-status :no-kvs}])
-      [state {:status :invalid :sub-status :no-kvs-ident}])))
+          [state {:status :mismatch :status-info :wrong-uuid}])
+        [state {:status :not-found :status-info :no-kvs}])
+      [state {:status :invalid :status-info :no-kvs-ident}])))
 
 (defn state-work [actx state-ch m work-fn & opt-done-ch]
-  (act state-work actx
-       (let [done-ch (or opt-done-ch (achan state-work))]
-         (aput state-work state-ch [work-fn done-ch])
+  (let [done-ch (or opt-done-ch (achan actx))]
+    (act state-work actx
+         (aput state-work state-ch [work-fn done-ch]))
          (aput state-work (:res-ch m)
-               (merge m (or (atake state-work done-ch) {:status :error}))))))
+               (merge m (or (atake state-work done-ch)
+                            {:status :error :status-info :closed-ch})))))
 
 (def op-handlers
   {:kvs-open
    (fn [actx state-ch m]
      (state-work actx state-ch m
-                 (kvs-check (:kvs-ident m)
-                            #(if-let [name (:name m)]
-                               (if-let [kvs (get-in % [:kvss name])]
-                                 [% {:status :ok :kvs-ident [name (:uuid kvs)]}]
-                                 [(-> %
-                                      (update-in [:kvss name]
-                                                 (make-kvs name (:next-uuid %)))
-                                      (assoc :next-uuid (inc (:next-uuid %))))
-                                  {:status :ok :kvs-ident [name (:next-uuid %)]}])
-                               [% {:status :invalid :sub-status :no-name}]))))
+                 #(if-let [name (:name m)]
+                    (if-let [kvs (get-in % [:kvss name])]
+                      [% {:status :ok :status-info :reopened
+                          :kvs-ident [name (:uuid kvs)]}]
+                      [(-> %
+                           (update-in [:kvss name] (make-kvs name (:next-uuid %)))
+                           (assoc :next-uuid (inc (:next-uuid %))))
+                       {:status :ok :status-info :created
+                        :kvs-ident [name (:next-uuid %)]}])
+                    [% {:status :invalid :status-info :no-name}])))
 
    :kvs-remove
    (fn [actx state-ch m]
      (state-work actx state-ch m
-                 (kvs-check (:kvs-ident m)
-                            (fn [state kvs]
-                              [(dissoc-in state [:kvss (:name kvs)])
-                               {:status :ok}]))))
+                 (kvs-ident-check (:kvs-ident m)
+                                  (fn [state kvs]
+                                    [(dissoc-in state [:kvss (:name kvs)])
+                                     {:status :ok :status-info :removed}]))))
 
    :multi-get
    (fn [actx state-ch m]
      (let [keys (:keys m)
            res-m (dissoc m :keys)
            res-ch (:res-ch m)
-           done-ch (achan actx)
            work-fn (fn [state kvs]
-                     [state
-                      (act multi-get actx
-                           (doseq [key (:keys m)]
-                             (if-let [entry (kvs-entry-by-key kvs key)]
-                               (aput multi-get res-ch
-                                     (merge res-m
-                                            {:partial :ok :key key :entry entry}))
-                               (aput multi-get res-ch
-                                     (merge res-m
-                                            {:partial :not-found :key key}))))
-                           (aput multi-get done-ch {:status :ok}))])]
-       (state-work actx state-ch m
-                   (kvs-check (:kvs-ident m) work-fn) done-ch)))
+                     (act multi-get actx
+                          (let [kc-clean (:clean kvs)]
+                            (doseq [key (:keys m)]
+                              (if-let [entry (kc-entry-by-key kc-clean key)]
+                                (aput multi-get res-ch
+                                      (merge res-m
+                                             {:partial :ok :key key :entry entry}))
+                                (aput multi-get res-ch
+                                      (merge res-m
+                                             {:partial :not-found :key key}))))
+                            (aput multi-get res-ch {:status :ok})))
+                     [state nil])]
+       (act multi-get-start actx
+         (aput multi-get-start state-ch [work-fn nil]))))
 
    :multi-change
    (fn [actx state-ch [res-ch name changes]])
@@ -121,10 +127,10 @@
 
     ; Using kvs-mgr-state to avoid atoms, which prevent time-travel.
     (act-loop kvs-mgr-state actx [state { :next-uuid 0 :kvss {} }]
-              (if-let [[state-fn res-ch] (atake kvs-mgr-state state-ch)]
-                (if-let [[next-state res] (state-fn state)]
-                  (do (when (and res-ch res)
-                        (aput-close kvs-mgr-state res-ch res))
+              (if-let [[state-fn done-ch] (atake kvs-mgr-state state-ch)]
+                (if-let [[next-state done-res] (state-fn state)]
+                  (do (when (and done-ch done-res)
+                        (aput-close kvs-mgr-state done-ch done-res))
                       (recur next-state))
                   (println :kvs-mgr-state-fn-exit))
                 (println :kvs-mgr-state-ch-exit)))
