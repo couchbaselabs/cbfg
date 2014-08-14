236acb7d0dffd0196e24eec4973fb3d3cc313f53
Steve Yen <steve.yen@gmail.com>
2014-08-14 08:30:46 -0700

    Better doc string for make-lane-pump.

--- src/cbfg/lane.cljs
+++ src/cbfg/lane.cljs
@@ -1,20 +1,19 @@
 (ns cbfg.lane
   (:require-macros [cbfg.act :refer [act-loop achan aclose aput atake]]))
 
-(defn make-lane-pump [actx in-ch out-ch make-lane-fn]
-  "Dispatches messages from in-ch to per-lane channels, based on an
-   optional :lane key in each message.  The make-lane-fn is used to
-   create a new per-lane worker channel for any as-yet unseen lane
-   name, and is invoked with parameters of [actx lane-name out-ch].  A
-   message is an associative map that looks like {:lane lane-name, :op
-   OP, ...}, where the :lane and :op keys are optional.  A special OP
-   of :close-lane will result in an out-ch response message that
-   includes a :status field of either :ok or :not-found, depending if
-   the named lane is known.  Although a client might close a lane,
-   there still might be inflight messages on the out-ch than arrive
-   'after' the lane closing, so an out-ch consumer should be prepared
-   to handle and/or drop those late messages.  To avoid confusion due
-   to this, a client should also avoid reusing lane names."
+(defn make-lane-pump
+  "Dispatches requests from in-ch to per-lane channels, based on an
+   optional :lane key in each request.  A lane-pump invokes the
+   make-lane-fn callback as needed to create a new per-lane worker
+   channel, with params of [actx lane-name out-ch].  A request message
+   is a map that looks like {:lane lane-name, :op OP, ...}.  A special
+   OP of :close-lane will have an out-ch response with a :status field
+   of either :ok or :not-found, depending if the named lane is known.
+   Although a client might close a lane, there still might be inflight
+   responses on the out-ch than arrive 'after' the lane closing, so an
+   out-ch consumer should handle and/or drop those late responses.
+   So, to avoid confusion, a client should avoid reusing lane-names."
+  [actx in-ch out-ch make-lane-fn]
   (act-loop lane-pump actx [lane-chs {} lane-chs-tot 0]
             (if-let [m (atake lane-pump in-ch)]
               (if (= (:op m) :close-lane)
