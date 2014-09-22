af959211d7758cd82c3739b447fd84cd31fd10bc
Steve Yen <steve.yen@gmail.com>
2014-09-22 10:59:34 -0700

    Use assoc instead of merge in kvs.

--- src/cbfg/kvs.cljs
+++ src/cbfg/kvs.cljs
@@ -48,9 +48,9 @@
       (if-let [kvs (get-in state [:kvss name])]
         (if (= uuid (:uuid kvs))
           (cb state kvs)
-          [state (merge m {:status :mismatch :status-info [:wrong-uuid (:kvs-ident m)]})])
-        [state (merge m {:status :not-found :status-info [:no-kvs (:kvs-ident m)]})])
-      [state (merge m {:status :invalid :status-info :missing-kvs-ident-arg})])))
+          [state (assoc m :status :mismatch :status-info [:wrong-uuid (:kvs-ident m)])])
+        [state (assoc m :status :not-found :status-info [:no-kvs (:kvs-ident m)])])
+      [state (assoc m :status :invalid :status-info :missing-kvs-ident-arg)])))
 
 (defn kvs-do [actx state-ch res-ch cb]
   (act kvs-do actx
@@ -68,9 +68,9 @@
                          (when-let [entry (get-entry kc k v)]
                            (when (or include-deleted (not (:deleted entry)))
                              (aput scan-work res-ch
-                                   (merge m {:more true :status :ok
-                                             res-key key :entry entry})))))
-                       (aput-close scan-work res-ch (merge m {:status :ok}))))
+                                   (assoc m :more true :status :ok
+                                          res-key key :entry entry)))))
+                       (aput-close scan-work res-ch (assoc m :status :ok))))
                 nil)]
        (kvs-do actx state-ch res-ch
                (fn [state] ((kvs-checker m cb) (or kvs-snapshot state)))))))
@@ -83,8 +83,8 @@
                (act list-names actx
                     (doseq [name (keys (:kvss state))]
                       (aput list-names (:res-ch m)
-                            (merge m {:more true :status :ok :name name})))
-                    (aput-close list-names (:res-ch m) (merge m {:status :ok})))
+                            (assoc m :more true :status :ok :name name)))
+                    (aput-close list-names (:res-ch m) (assoc m :status :ok)))
                nil)))
 
    :kvs-open
@@ -92,25 +92,21 @@
      (kvs-do actx state-ch (:res-ch m)
              #(if-let [name (:name m)]
                 (if-let [kvs (get-in % [:kvss name])]
-                  [% (merge m {:status :ok
-                               :status-info :reopened
-                               :kvs-ident [name (:uuid kvs)]})]
+                  [% (assoc m :status :ok :status-info :reopened
+                            :kvs-ident [name (:uuid kvs)])]
                   [(-> %
                        (assoc-in [:kvss name] (make-kvs name (:next-uuid %)))
                        (assoc :next-uuid (inc (:next-uuid %))))
-                   (merge m {:status :ok
-                             :status-info :created
-                             :kvs-ident [name (:next-uuid %)]})])
-                [% (merge m {:status :invalid
-                             :status-info :missing-name-arg})])))
+                   (assoc m :status :ok :status-info :created
+                          :kvs-ident [name (:next-uuid %)])])
+                [% (assoc m :status :invalid :status-info :missing-name-arg)])))
 
    :kvs-remove
    (fn [actx state-ch m]
      (kvs-do actx state-ch (:res-ch m)
              (kvs-checker m (fn [state kvs]
                               [(dissoc-in state [:kvss (:name kvs)])
-                               (merge m {:status :ok
-                                         :status-info :removed})]))))
+                               (assoc m :status :ok :status-info :removed)]))))
 
    :multi-get
    (fn [actx state-ch m]
@@ -123,14 +119,12 @@
                          (let [entry (kc-entry-by-key kc key)]
                            (if (and entry (or include-deleted (not (:deleted entry))))
                              (aput multi-get res-ch
-                                   (merge res-m {:more true
-                                                 :status :ok
-                                                 :key key :entry entry}))
+                                   (assoc res-m :more true :status :ok
+                                          :key key :entry entry))
                              (aput multi-get res-ch
-                                   (merge res-m {:more true
-                                                 :status :not-found
-                                                 :key key})))))
-                       (aput-close multi-get res-ch (merge res-m {:status :ok}))))
+                                   (assoc res-m :more true :status :not-found
+                                          :key key)))))
+                       (aput-close multi-get res-ch (assoc res-m :status :ok))))
                 nil)]
        (kvs-do actx state-ch res-ch
                (fn [state] ((kvs-checker m cb) (or kvs-snapshot state))))))
@@ -149,7 +143,7 @@
                   (act multi-change actx
                        (doseq [res ress]
                          (aput multi-change res-ch (merge res-m res)))
-                       (aput-close multi-change res-ch (merge res-m {:status :ok})))
+                       (aput-close multi-change res-ch (assoc res-m :status :ok)))
                   [(assoc-in state [:kvss (:name kvs)] next-kvs) nil]))]
        (kvs-do actx state-ch res-ch (kvs-checker m cb))))
 
@@ -162,7 +156,7 @@
    :snapshot
    (fn [actx state-ch m]
      (kvs-do actx state-ch (:res-ch m)
-             (fn [state] [state (merge m {:status :ok :kvs-snapshot state})])))
+             (fn [state] [state (assoc m :status :ok :kvs-snapshot state)])))
 
    :sync
    (fn [actx state-ch m]
@@ -172,8 +166,8 @@
                                          (-> kvs
                                              (assoc :clean (:dirty kvs))
                                              (assoc :dirty (make-kc))))
-                               (merge m {:status :ok
-                                         :status-info [:synced (:kv-ident m)]})]))))})
+                               (assoc m :status :ok
+                                      :status-info [:synced (:kv-ident m)])]))))})
 
 (defn make-kvs-mgr [actx & {:keys [cmd-ch state-ch op-handlers]}]
   (let [cmd-ch (or cmd-ch (achan actx))
