a790b840824d7a7a905f808410f6e0443b5781f9
Steve Yen <steve.yen@gmail.com>
2014-10-11 09:46:09 -0700

    Test max-lanes.

--- src/cbfg/test/lane.cljs
+++ src/cbfg/test/lane.cljs
@@ -21,73 +21,101 @@
 
 (defn test-lane-pump [actx]
   (act tlp actx
-       (let [n (atom 0)
-             in-ch (achan actx)
-             out-ch (achan actx)
-             g (make-lane-pump tlp in-ch out-ch make-echo-lane)]
-         (if (and (do (aput tlp in-ch {:lane :a :op :hi :x 1})
-                      (e n
-                         (atake tlp out-ch)
-                         [:a 0 {:lane :a :op :hi :x 1}]
-                         nil))
-                  (do (aput tlp in-ch {:lane :b :x 2})
-                      (e n
-                         (atake tlp out-ch)
-                         [:b 0 {:lane :b :x 2}]
-                         nil))
-                  (do (aput tlp in-ch {:x 3})
-                      (e n
-                         (atake tlp out-ch)
-                         [nil 0 {:x 3}]
-                         nil))
-                  (do (aput tlp in-ch {:lane :a :x 10})
-                      (e n
-                         (atake tlp out-ch)
-                         [:a 1 {:lane :a :x 10}]
-                         nil))
-                  (do (aput tlp in-ch {:x 30})
-                      (e n
-                         (atake tlp out-ch)
-                         [nil 1 {:x 30}]
-                         nil))
-                  (do (aput tlp in-ch {:lane :x :op :lane-close})
-                      (e n
-                         (atake tlp out-ch)
-                         {:lane :x :op :lane-close :status :not-found}
-                         nil))
-                  (do (aput tlp in-ch {:lane :a :x 100})
-                      (e n
-                         (atake tlp out-ch)
-                         [:a 2 {:lane :a :x 100}]
-                         nil))
-                  (do (aput tlp in-ch {:lane :a :op :lane-close})
-                      (e n
-                         (atake tlp out-ch)
-                         {:lane :a :op :lane-close :status :ok}
-                         nil))
-                  (e n
-                     (atake tlp out-ch) ; For lane :a.
-                     :done
-                     nil)
-                  (do (aput tlp in-ch {:lane :a :x 1000})
-                      (e n
-                         (atake tlp out-ch)
-                         [:a 0 {:lane :a :x 1000}]
-                         nil))
-                  (do (aclose tlp in-ch)
-                      true)
-                  (e n
-                     (atake tlp out-ch) ; For lane :a.
-                     :done
-                     nil)
-                  (e n
-                     (atake tlp out-ch) ; For lane :b.
-                     :done
-                     nil)
-                  (e n
-                     (atake tlp out-ch) ; For lane nil.
-                     :done
-                     nil))
+       (let [n (atom 0)]
+         (if (and
+              (let [in-ch (achan actx)
+                    out-ch (achan actx)
+                    g (make-lane-pump tlp in-ch out-ch make-echo-lane)]
+                (and (do (aput tlp in-ch {:lane :a :op :hi :x 1})
+                         (e n
+                            (atake tlp out-ch)
+                            [:a 0 {:lane :a :op :hi :x 1}]
+                            nil))
+                     (do (aput tlp in-ch {:lane :b :x 2})
+                         (e n
+                            (atake tlp out-ch)
+                            [:b 0 {:lane :b :x 2}]
+                            nil))
+                     (do (aput tlp in-ch {:x 3})
+                         (e n
+                            (atake tlp out-ch)
+                            [nil 0 {:x 3}]
+                            nil))
+                     (do (aput tlp in-ch {:lane :a :x 10})
+                         (e n
+                            (atake tlp out-ch)
+                            [:a 1 {:lane :a :x 10}]
+                            nil))
+                     (do (aput tlp in-ch {:x 30})
+                         (e n
+                            (atake tlp out-ch)
+                            [nil 1 {:x 30}]
+                            nil))
+                     (do (aput tlp in-ch {:lane :x :op :lane-close})
+                         (e n
+                            (atake tlp out-ch)
+                            {:lane :x :op :lane-close :status :not-found}
+                            nil))
+                     (do (aput tlp in-ch {:lane :a :x 100})
+                         (e n
+                            (atake tlp out-ch)
+                            [:a 2 {:lane :a :x 100}]
+                            nil))
+                     (do (aput tlp in-ch {:lane :a :op :lane-close})
+                         (e n
+                            (atake tlp out-ch)
+                            {:lane :a :op :lane-close :status :ok}
+                            nil))
+                     (e n
+                        (atake tlp out-ch) ; For lane :a.
+                        :done
+                        nil)
+                     (do (aput tlp in-ch {:lane :a :x 1000})
+                         (e n
+                            (atake tlp out-ch)
+                            [:a 0 {:lane :a :x 1000}]
+                            nil))
+                     (do (aclose tlp in-ch)
+                         true)
+                     (e n
+                        (atake tlp out-ch) ; For lane :a.
+                        :done
+                        nil)
+                     (e n
+                        (atake tlp out-ch) ; For lane :b.
+                        :done
+                        nil)
+                     (e n
+                        (atake tlp out-ch) ; For lane nil.
+                        :done
+                        nil)))
+              ; Test max-lanes.
+              (let [in-ch (achan actx)
+                    out-ch (achan actx)
+                    g (make-lane-pump tlp in-ch out-ch make-echo-lane
+                                      :max-lanes 0)]
+                (do (aput tlp in-ch {:lane :a :op :hi :x 1})
+                    (e n
+                       (atake tlp out-ch)
+                       {:lane :a :op :hi :x 1
+                        :status :full :status-info :max-lanes}
+                       nil)))
+              (let [in-ch (achan actx)
+                    out-ch (achan actx)
+                    g (make-lane-pump tlp in-ch out-ch make-echo-lane
+                                      :max-lanes 1)]
+                (and
+                 (do (aput tlp in-ch {:lane :a :op :hi :x 1})
+                         (e n
+                            (atake tlp out-ch)
+                            [:a 0 {:lane :a :op :hi :x 1}]
+                            nil))
+                 (do (aput tlp in-ch {:lane :b :op :hi :x 2})
+                     (e n
+                        (atake tlp out-ch)
+                        {:lane :b :op :hi :x 2
+                         :status :full :status-info :max-lanes}
+                        nil)))))
            "pass"
            (str "FAIL: on test-lane #" @n)))))
 
